e409397afd0f61c34832dbc70cf64b3a0df2ad29
sarthakp972 <143208467+sarthakp972@users.noreply.github.com>
2024-02-12 21:13:42 +0530

    12feb

--- reactjsgps/src/App.jsx
+++ reactjsgps/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Button from 'react-bootstrap/Button';
